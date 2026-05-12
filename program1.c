6b0166cdffab9755626c357a845ffff390bd681e
Srushti Pagar <srushtipagar814@gmail.com>
2026-05-12 21:30:04 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>   //header file
+
+int main()      //entry point function
+{
+    printf("Jay Ganesh...\n");      //console vr print 
+
+    return 0;
+}
+
+// cui and gui
+//command/console/character user interface
+//graphical user interface
