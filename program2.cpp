d3a9ff9efa6b602a00dac02eb47b593f308b69cf
Srushti Pagar <srushtipagar814@gmail.com>
2026-05-12 21:32:51 +0530

    Add files via upload

---
+++ program2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>      //header file
+using namespace std;    //std namespace
+
+int main()
+{
+    cout<<"Jay Ganesh..."<<endl;
+
+    return 0;       //successful code indication to os
+}
