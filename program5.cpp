df5f4d159285b9b9807840d9d9cd322479b55ba0
Srushti Pagar <srushtipagar814@gmail.com>
2026-05-22 13:57:57 +0530

    Add files via upload

---
+++ program5.cpp
@@ -0,0 +1,29 @@
+/*
+Algorithm
+    Start   
+        Accept 1st number as No1    //float 
+        Accept 2nd number as No2    //float
+        Perform Addition of No1 and No2
+        Display the result
+    Stop
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float i ,j ,k;
+
+    cout<<"Enter First Number :\n";
+    cin>> i;
+
+    cout<<"Enter Seconf Number :\n";
+    cin>>j;
+
+    k = i + j;
+
+    cout<<"Addition is : \n"<<k;
+
+    return 0;
+}
