9c206e210d2f1886e597728a5ef2fd6e87ae4bd5
Srushti Pagar <srushtipagar814@gmail.com>
2026-05-22 13:57:18 +0530

    Add files via upload

---
+++ program4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i , j , k;
+
+    cout<<"Enter first number : \n";
+    cin>>i;
+
+    cout<<"Enter second number : \n";
+    cin>>j;
+
+    k = i + j;
+
+    cout<<"Addition is : "<<k;
+
+    return 0;
+}
