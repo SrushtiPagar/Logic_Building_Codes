7fea2f3c5c26465c425857fce7a7d9c09806f55d
Srushti Pagar <srushtipagar814@gmail.com>
2026-05-24 23:36:09 +0530

    Add files via upload

---
+++ program17.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdbool.h>         //for boolean datatype because c do not have boolean datatype
+
+//check whether = boolean value
+
+bool CheckEvenOdd(int iNo)
+{
+    if (iNo % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;           //iret = integer return
+
+    printf("Enter Number to Check whether it is Even or Odd: ");
+    scanf("%d" , &iValue);
+
+    bRet = CheckEvenOdd(iValue);
+
+    //output should be descriptive
+    if(bRet)
+    {
+        printf("%d is Even \n",iValue);
+    }
+    else
+    {
+        printf("%d is Odd \n" , iValue);
+    }
+
+    return 0;
+}
