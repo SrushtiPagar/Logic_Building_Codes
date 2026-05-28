8a5b4a06702017bb8e7d22b3eef8f75b3efbba1c
Srushti Pagar <srushtipagar814@gmail.com>
2026-05-28 19:47:45 +0530

    Add files via upload

---
+++ program25.c
@@ -0,0 +1,18 @@
+//using DRY policy 
+//Don't repeat yourself
+
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+
+    // Iteration
+
+    for(int iCnt = 1 ; iCnt <= 4 ; iCnt++)
+    {
+        printf("Jay Ganesh...\n");
+    }
+
+    return 0;
+}
