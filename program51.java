5cefa3ffa08ba6e0ffeac7baed9da4c62e4abdc1
Srushti Nitin Pagar <srushtipagar814@gmail.com>
2026-06-27 23:16:07 +0530

    Add files via upload

---
+++ program51.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+class NumberX
+{
+   public boolean CheckPerfect(int iNo)
+   {
+        //Logic
+        int iCnt = 0;
+        int iSum = 0;
+
+        for(iCnt = 1 ; iCnt <= (iNo / 2) ; iCnt++)
+        {
+            if((iNo % iCnt) == 0)
+            {
+                iSum += iCnt;
+                if(iSum > iNo)
+                {
+                    break;
+                }
+            }
+        }
+        if(iSum == iNo)
+        {
+            return true;
+        }
+        else
+            return false;
+   }
+}
+
+class program51
+{
+    
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0;
+        boolean bRet = false;
+
+        System.out.println("Enter a number  : ");
+        iValue = sobj.nextInt();
+
+        NumberX nobj = new NumberX();
+        bRet = nobj.CheckPerfect(iValue);
+
+        if(bRet == true)
+            System.out.println(iValue+" is a Perfect Number");
+        else
+            System.out.print(iValue+" is NOT perfect number");
+
+        sobj.close();
+    }    
+}
+//Time Complexity : O(N/2)
+//Where N>=0; program51 {
+    
+
