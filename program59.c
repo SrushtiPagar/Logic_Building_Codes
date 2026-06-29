4c2a6ed025315d306239317788578aeac71770c6
Srushti Nitin Pagar <srushtipagar814@gmail.com>
2026-06-29 22:43:52 +0530

    Add files via upload

---
+++ program59.c
@@ -0,0 +1,32 @@
+//Input  : 5
+//Output : 5 4 3 2 1 
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(int iCnt = iNo ; iCnt >= 1 ; iCnt--)
+    {
+        printf("%d \t" , iCnt);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue  = 0;
+
+    printf("Enter Number : ");
+    scanf("%d" , &iValue);
+
+    Display(iValue);
+}
+
+// 2nd Approach
+// for(int iCnt = 1 ; iCnt < iNo ; iCnt++)
+//     {
+//         printf("%d \t" , iCnt);
+//         iCnt++;
+//     }
